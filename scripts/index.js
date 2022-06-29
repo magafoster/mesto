595522c2e49a5406a2815e6e79d4ff7d67f3bc5f
Maga Foster <i@mfoster.ru>
2022-06-30 01:39:59 +0300

    2 иттерация понять все

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,33 @@
+let profileEditButton = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
 let popupElem = document.querySelector('.popup');
 let popupContainer = popupElem.querySelector('.popup__container');
 let popupForm = popupElem.querySelector('.popup__form');
 let popupClose = popupElem.querySelector('.popup__close-button');
 let popupInput = popupElem.querySelectorAll('.popup__input_type_text');
 
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let profileEditButton = document.querySelector('.profile__edit-button');
-
 let inputName = popupInput[0];
 let inputAbout = popupInput[1];
-inputName.value = profileName.textContent;
-inputAbout.value = profileAbout.textContent;
-
-popupClose.addEventListener('click', closePopup);
-
-profileEditButton.addEventListener('click', openPopup);
-popupForm.addEventListener('submit', formSubmitHandler);
-
-function closePopup () {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-  popupElem.style.display = 'none';
-}
 
 function openPopup () {
-  popupElem.style.display = 'flex';
+  popupElem.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
+}
+
+function closePopup () {
+  popupElem.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  popupElem.style.display = 'none';
+  closePopup ();
 }
 
-
+popupClose.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+popupForm.addEventListener('submit', formSubmitHandler);
