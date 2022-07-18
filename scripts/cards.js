d94f5c54d7e365863de56caa3f91bc314fb5a6cc
Maga Foster <i@mfoster.ru>
2022-07-18 17:43:08 +0300

    5ПР 2-я попытка

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Лондон",
+    link: "./images/London.jpg",
+  },
+  {
+    name: "Лос-Анджелес",
+    link: "./images/Los_Angeles.jpg",
+  },
+  {
+    name: "Милан",
+    link: "./images/Milano.jpg",
+  },
+  {
+    name: "Москва",
+    link: "./images/Moscow.jpg",
+  },
+  {
+    name: "Токио",
+    link: "./images/Tokyo.jpg",
+  },
+  {
+    name: "Дубай",
+    link: "./images/Dubai.jpg",
+  },
+];
